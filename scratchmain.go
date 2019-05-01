f2f18a89847f4e82448467fcdc8cafd6f69b33db
Ken Dobbins <kdobbins@op5.com>
2019-05-01 14:00:08 -0500

    Added difference of Get Method to test

--- scratchmain.go
+++ scratchmain.go
@@ -11,7 +11,7 @@ import (
 func main() {
 	start := time.Now()
 
-	count := 30
+	count := 300
 	then := start.Add(time.Duration(-count) * time.Second).Unix()
     timeFilter := fmt.Sprintf("last_check > %d AND ", then)
 
@@ -20,7 +20,7 @@ func main() {
 	defer c.Close()
 
 	q := livestatus.NewQuery("services")
-	q.Columns("acknowledged, acknowledgement_type, check_command, check_freshness, check_type, custom_variable_names, custom_variable_values, custom_variables, description, display_name, downtimes_with_info, execution_time, groups, host_name, host_acknowledged, host_acknowledgement_type, host_address, host_alias, host_check_freshness, host_check_type, host_childs, host_custom_variable_names, host_custom_variable_values, host_custom_variables, host_display_name, host_downtimes_with_info, host_groups, host_hard_state, host_is_flapping, host_last_check, host_last_hard_state_change, host_last_hard_state, host_last_state_change, host_last_state, host_last_time_down, host_last_time_unreachable, host_last_time_up, host_latency, host_long_plugin_output, host_num_services_crit, host_num_services_hard_crit, host_num_services_hard_ok, host_num_services_hard_unknown, host_num_services_hard_warn, host_num_services_ok, host_num_services_pending, host_num_services_unknown, host_num_services_warn, host_num_services, host_parents, host_pending_flex_downtime, host_percent_state_change, host_perf_data, host_plugin_output, host_scheduled_downtime_depth, host_state_type, host_state, host_total_services, host_worst_service_hard_state, host_worst_service_state, is_flapping, last_check, last_hard_state_change, last_hard_state, last_state_change, last_state, last_time_critical, last_time_ok, last_time_unknown, last_time_warning, latency, long_plugin_output, percent_state_change, perf_data, plugin_output, scheduled_downtime_depth, state_type, state")
+	q.Columns("description", "host_name", "host_is_flapping", "last_check", "host_groups", "state", "host_num_services")
 	q.Filter(timeFilter)
 	q.Filter("state = 0")
 
@@ -31,42 +31,78 @@ func main() {
 	}
 
 	for _, r := range resp.Records {
-		host_name, err := r.GetString("host_name")
+		host_name_plain, err := r.Get("host_name")
 		if err != nil {
 			fmt.Fprintf(os.Stderr, "Warning: %s", err)
 		}
 
-		hostNumServices, err := r.GetInt("host_num_services")
+		hostNumServices_plain, err := r.Get("host_num_services")
 		if err != nil {
 			fmt.Fprintf(os.Stderr, "Warning: %s", err)
 		}
 
-		description, err := r.GetString("description")
+		description_plain, err := r.Get("description")
 		if err != nil {
 			fmt.Fprintf(os.Stderr, "Warning: %s", err)
 		}
 
-		state, err := r.GetInt("state")
+		state_plain, err := r.Get("state")
 		if err != nil {
 			fmt.Fprintf(os.Stderr, "Warning: %s", err)
 		}
 
-		hostIsFlapping, err := r.GetBool("host_is_flapping")
+		hostIsFlapping_plain, err := r.Get("host_is_flapping")
 		if err != nil {
 			fmt.Fprintf(os.Stderr, "Warning: %s", err)
 		}
 
-		lastCheck, err := r.GetTime("last_check")
+		lastCheck_plain, err := r.Get("last_check")
 		if err != nil {
 			fmt.Fprintf(os.Stderr, "Warning: %s", err)
 		}
 
-		hostGroups, err := r.GetSlice("host_groups")
+		hostGroups_plain, err := r.Get("host_groups")
+		if err != nil{
+			fmt.Fprintf(os.Stderr, "Warning: %s", err)
+		}
+		host_name_string, err := r.GetString("host_name")
+		if err != nil {
+			fmt.Fprintf(os.Stderr, "Warning: %s", err)
+		}
+
+		hostNumServices_int, err := r.GetInt("host_num_services")
+		if err != nil {
+			fmt.Fprintf(os.Stderr, "Warning: %s", err)
+		}
+
+		description_string, err := r.GetString("description")
+		if err != nil {
+			fmt.Fprintf(os.Stderr, "Warning: %s", err)
+		}
+
+		state_int, err := r.GetInt("state")
+		if err != nil {
+			fmt.Fprintf(os.Stderr, "Warning: %s", err)
+		}
+
+		hostIsFlapping_bool, err := r.GetBool("host_is_flapping")
+		if err != nil {
+			fmt.Fprintf(os.Stderr, "Warning: %s", err)
+		}
+
+		lastCheck_time, err := r.GetTime("last_check")
+		if err != nil {
+			fmt.Fprintf(os.Stderr, "Warning: %s", err)
+		}
+
+		hostGroups_slice, err := r.GetSlice("host_groups")
 		if err != nil{
 			fmt.Fprintf(os.Stderr, "Warning: %s", err)
 		}
 
 
-		fmt.Printf("Host: %s has %d services.\n    The Service %s is currently in state: %d,\n    Is the host flapping? %t\n    Last Service Check was performed at %s\n    The host is in the following hostgroups: %v\n", host_name, hostNumServices, description, state, hostIsFlapping, lastCheck, hostGroups)
+		fmt.Printf("\n\nUsing Plain Get Method\nHostname: %s\n\tHostNumServices: %f\n\tDescription: %s\n\tState: %f\n\tFlapping: %f\n\tLastCheck: %f\n\tHostGroups: %v\n", host_name_plain, hostNumServices_plain, description_plain, state_plain, hostIsFlapping_plain, lastCheck_plain, hostGroups_plain)
+		fmt.Printf("\n\nUsing Specific Get Method\nHostname: %s\n\tHostNumServices: %d\n\tDescription: %s\n\tState: %d\n\tFlapping: %t\n\tLastCheck: %s\n\tHostGroups: %v\n", host_name_string, hostNumServices_int, description_string, state_int, hostIsFlapping_bool, lastCheck_time, hostGroups_slice)
+
 	}
 }
